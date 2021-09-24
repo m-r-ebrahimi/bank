bf7f2512df7dc5373e3c34490e976e3f2c6db2d9
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-24 14:37:08 +0330

    Initialization

---
+++ src/main/java/database/DbConfig.java
@@ -0,0 +1,8 @@
+package database;
+
+public class DbConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/bank";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+}
